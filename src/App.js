cf662dd3b0a8d915dd8c416e0dedcb7196b35e37
Muhammed Ameen <ameens.in19@gmail.com>
2025-03-21 19:26:43 +0530

    :deploy

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DonationPage from './components/DonationPage';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<DonationPage />} />
-      </Routes>
-    </Router>
-  );
+  return <DonationPage />;
 }
 
 export default App;
